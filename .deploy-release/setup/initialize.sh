b764d13b0c86ab89f2b357da747bf993d161c627
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-07-12 17:21:15 -0600

    chore: try to fix trame deploy

--- .deploy-release/setup/initialize.sh
+++ .deploy-release/setup/initialize.sh
@@ -1,2 +1,2 @@
 pip install trame-mnist
-pip install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/cu121
+pip install -U --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/cu121
